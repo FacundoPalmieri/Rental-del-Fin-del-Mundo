d62f28b0a0bfcdfc22ff961d25ac674738b6cecd
frios <frios@jst.gob.ar>
2024-04-08 16:54:12 -0300

    calendario en admin

--- rental/rental/admin.py
+++ rental/rental/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from .models import *
+from django.urls import reverse
+import json
 
 admin.site.register(Plan)
 admin.site.register(Auto)
-admin.site.register(Rental)
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/rental_change_list.html'
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request, extra_context=extra_context,
+        )
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        # Customize this according to your Rental model
+        rentals_data = [{'title': f"{rental.auto} / {rental.nombre} {rental.apellido}",
+                        'start': rental.fecha_retiro.strftime('%Y-%m-%d'),
+                        'end': rental.fecha_devolucion.strftime('%Y-%m-%d') if rental.fecha_devolucion else None,
+                        'url': reverse('admin:rental_rental_change', args=(rental.pk,))}
+                        for rental in qs]
+
+        response.context_data['rentals_json'] = json.dumps(rentals_data)
+
+        return response
